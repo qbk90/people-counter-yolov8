2db6cbc63346f0d8c8afdcb45db687be8b3aefbb
qibeike <39752005+qibeike@users.noreply.github.com>
2024-03-06 15:37:15 -0300

    added comments for clarity

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pandas as pd
 import numpy as np
 from ultralytics import YOLO
 
-
+# Preditor model instance creation
 model=YOLO('yolov8s.pt')
 
+# Run the model on the GPU instead of CPU
+model.to("cuda")
 
+# Areas of interest coordinates
 area1=[(312,388),(289,390),(474,469),(497,462)]
 
 area2=[(279,392),(250,397),(423,477),(454,469)]
+
+# Function to get the coordinated of the mouse cursor when
+# the cursor is inside the RGB window
 def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE :  
         colorsBGR = [x, y]
@@ -21,15 +27,15 @@ cv2.setMouseCallback('RGB', RGB)
 
 cap=cv2.VideoCapture('peoplecount1.mp4')
 
-
+# Classes of interest list creation
 my_file = open("classes.txt", "r")
-data = my_file.read()
-class_list = data.split("\n") 
-#print(class_list)
+class_list = my_file.read().split("\n")
+
+# print(class_list)
 
 count=0
 
-
+# Run until Esc key is pressed
 while True:    
     ret,frame = cap.read()
     if not ret:
@@ -39,13 +45,18 @@ while True:
         continue
     frame=cv2.resize(frame,(1020,500))
 #    frame=cv2.flip(frame,1)
+    
+    # Store the predicted objects data into a
+    # Pandas dataframe
     results=model.predict(frame)
- #   print(results)
+    # print(results)   
     a=results[0].boxes.data
-    px=pd.DataFrame(a).astype("float")
-#    print(px)
+    px=pd.DataFrame(a.cpu().numpy()).astype("float")
+    # print(px)
     list=[]
-             
+    
+    # If the detected object is a person,
+    # draw a rectangle around them
     for index,row in px.iterrows():
 #        print(row)
  
@@ -53,9 +64,9 @@ while True:
         y1=int(row[1])
         x2=int(row[2])
         y2=int(row[3])
-        d=int(row[5])
-        c=class_list[d]
-        if 'person' in c:
+        obj_class=int(row[5])
+        c=class_list[obj_class]
+        if c == "person":
            cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
            cv2.putText(frame,str(c),(x1,y1),cv2.FONT_HERSHEY_COMPLEX,(0.5),(255,255,255),1)
         
